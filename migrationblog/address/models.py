a9ab6fa413eec337b599c42fe4186651e574273d
Marie Engel <marie.engel@deliveryhero.com>
2022-02-02 12:22:32 +0100

    add field to existing model

--- migrationblog/address/models.py
+++ migrationblog/address/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class AddressBook(models.Model):
     name = models.CharField(max_length=200)
     phone = models.CharField(max_length=25)
+    address =  models.CharField(max_length=250, default='no address recorded')  # new column, default takes care of records from before without addressed 
