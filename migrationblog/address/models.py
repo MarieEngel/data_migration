2623fd6271ac1f317e1b1b877532c1148dec0788
Marie Engel <marie.engel@deliveryhero.com>
2022-02-02 13:15:55 +0100

    datamigration

--- migrationblog/address/models.py
+++ migrationblog/address/models.py
@@ -4,4 +4,12 @@ from django.db import models
 class AddressBook(models.Model):
     name = models.CharField(max_length=200)
     phone = models.CharField(max_length=25)
-    address =  models.CharField(max_length=250, default='no address recorded')  # new column, default takes care of records from before without addressed 
+    # 8) remove address field, makemigrations, migrate:
+    # address =  models.CharField(max_length=250, default='no address recorded')  # 0) new column, default takes care of records from before without addressed 
+    #  to save postal code from address field to new field 'postcode' and get rid of address field:
+    # 1) create new field, makemigration, 
+   
+    postcode = models.CharField(max_length=15, default = "00000")
+    # 2) to get postal code from address: data migration: makemigrations --empty address -> rename migration file
+    # 3) -> migration file
+    
